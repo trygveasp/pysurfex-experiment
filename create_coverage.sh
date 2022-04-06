72429989ad06318ae6bee4fbfe099eb8dc934ea3
Trygve Aspelien <trygveasp@met.no>
2022-04-06 12:24:45 +0200

    Revert back to nose for coverage as it works for surfex

--- create_coverage.sh
+++ create_coverage.sh
@@ -6,14 +6,8 @@ set -x
 export PATH=$PWD/test/bin:/usr/bin/:$HOME/.local/bin:$PATH
 
 
-#./prepare_testdata.sh 
-#nosetests --with-timer --with-coverage --cover-erase --cover-html --cover-html-dir=coverage \
-#--cover-package=experiment \
-#test/test_submission.py \
-#test/test_ecflow.py \
-#test/test_tasks.py \
-#|| exit 1
+nosetests --with-timer --with-coverage --cover-erase --cover-html --cover-html-dir=coverage \
+--cover-package=experiment \
+test/test_tasks.py \
+|| exit 1
 
-cd test
-coverage run --source=.. -m unittest test_tasks.py
-coverage html
