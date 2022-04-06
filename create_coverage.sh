bbce7a704e423052c47613cb90542182a73f9f7d
Trygve Aspelien <trygveasp@met.no>
2022-04-06 12:35:07 +0200

    Remove timer

--- create_coverage.sh
+++ create_coverage.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-
 set -x
 
-export PATH=$PWD/test/bin:/usr/bin/:$HOME/.local/bin:$PATH
-
-
-nosetests --with-timer --with-coverage --cover-erase --cover-html --cover-html-dir=coverage \
+export PATH=$PWD/test/bin:/usr/bin/:$PATH
+nosetests --with-coverage --cover-erase --cover-html --cover-html-dir=coverage \
 --cover-package=experiment \
 test/test_tasks.py \
 || exit 1
