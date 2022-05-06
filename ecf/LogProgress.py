5231485362e71407091669f9500ef6b625b112a6
Trygve Aspelien <trygveasp@met.no>
2022-05-06 20:42:44 +0000

    Split out LogProgress in separate tasks as they must run and work in background for exp_dir

---
+++ ecf/LogProgress.py
@@ -0,0 +1,45 @@
+import sys
+lib = "%EXP_DIR%"
+sys.path.insert(0, lib)
+import scheduler
+import json
+import os
+import subprocess
+
+print("ECF_JOB_CMD: %ECF_JOB_CMD%")
+print(scheduler.__file__)
+
+exp_name = "%EXP%"
+server_logfile = "%SERVER_LOGFILE%"
+stream = "%STREAM%"
+if stream == "":
+    stream = None
+
+server = scheduler.EcflowServerFromFile(lib + "/Env_server", server_logfile)
+
+ecf_name = "%ECF_NAME%"
+ecf_pass = "%ECF_PASS%"
+ecf_tryno = "%ECF_TRYNO%"
+ecf_rid = "%ECF_RID%"
+next_dtg = "%DTG_NEXT%"
+dtgbeg = "%DTGBEG%"
+submission_id = "%SUBMISSION_ID%"
+
+# TODO stream
+stream = None
+
+task = scheduler.EcflowTask(ecf_name, ecf_tryno, ecf_pass, ecf_rid, submission_id)
+
+# This will also handle call to sys.exit(), i.e. Client.__exit__ will still be called.
+with scheduler.EcflowClient(server, task) as ci:
+
+    system = json.load(open(lib + "/exp_system.json", "r"))
+    wd = system["0"]["exp_dir"]
+    st = ""
+    if stream is not None and stream != "":
+        st = "_stream_" + stream
+    progress_file = wd + "/progress" + st + ".json"
+    # Update progress
+    progress = {"DTG": next_dtg, "DTGBEG": dtgbeg}
+    json.dump(progress, open(progress_file, "w"), indent=2)
+
