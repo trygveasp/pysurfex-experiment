ee26c179756bce19f7ea780db9a43c37faee42c9
Trygve Aspelien <trygveasp@met.no>
2023-01-16 11:20:11 +0100

    Get default submit arguments from environment/config

--- experiment_scheduler/cli.py
+++ experiment_scheduler/cli.py
@@ -1,6 +1,8 @@
 """Command line interfaces for starting/checking/stating jobs."""
+import os
 import sys
 import logging
+import shutil
 from argparse import ArgumentParser
 import experiment_scheduler as scheduler
 import experiment
@@ -11,10 +13,10 @@ def parse_submit_cmd_exp(argv):
     parser = ArgumentParser("ECF_submit task to ecflow")
     parser.add_argument('-config', dest="config_file", type=str, help="Configuration file")
     parser.add_argument('-task', type=str, help="Task name")
-    parser.add_argument('-task_job', type=str, help="Task job file")
-    parser.add_argument('-output', type=str, help="Output file")
-    parser.add_argument('-template', dest="template_job", type=str, help="Template")
-    parser.add_argument('-troika', type=str, help="Troika", default="troika", required=False)
+    parser.add_argument('-task_job', type=str, help="Task job file", required=False, default=None)
+    parser.add_argument('-output', type=str, help="Output file", required=False, default=None)
+    parser.add_argument('-template', dest="template_job", type=str, help="Template", required=False, default=None)
+    parser.add_argument('-troika', type=str, help="Troika", required=False, default=None)
     parser.add_argument('--debug', dest="debug", action="store_true", help="Debug information")
     # parser.add_argument('--version', action='version', version=__version__)
 
@@ -43,18 +45,53 @@ def submit_cmd_exp(**kwargs):
 
     logging.debug("kwargs %s", str(kwargs))
     logging.debug("scheduler location: %s", scheduler.__file__)
-    config = kwargs.get("config_file")
-    config = experiment.ConfigurationFromJsonFile(config)
+    config_file = kwargs.get("config_file")
+    cwd = os.getcwd()
+    if config_file is None:
+       config_file = f"{cwd}/exp_configuration.json"
+       logging.info("Using config file=%s", config_file)
+       if os.path.exists("exp_configuration.json"):
+          logging.info("Using config file=%s", config_file)
+       else:
+          raise FileNotFoundError("Could not find config file " + config_file)
+    config = experiment.ConfigurationFromJsonFile(config_file)
+    task = kwargs.get("task")
+    troika = kwargs.get("troika")
+    if troika is None:
+        try:
+            troika = config.system.get_var("TROIKA", "0")
+        except Exception:
+            troika = shutil.which("troika")
     troika_config = config.get_setting("TROIKA#CONFIG")
+    template_job = kwargs.get("template_job")
+    if template_job is None:
+        scripts = config.get_setting("GENERAL#PYSURFEX_EXPERIMENT")
+        if scripts is None:
+            raise Exception("Could not find GENERAL#PYSURFEX_EXPERIMENT")
+        else:
+            template_job = f"{scripts}/ecf/stand_alone.py"
+    task_job = kwargs.get("task_job")
+    if task_job is None:
+        task_job = f"{cwd}/{task}.job"
+    output = kwargs.get("output")
+    if output is None:
+        output = f"{cwd}/{task}.log"
+    logging.debug("Task: %s", task)
+    logging.debug("config: %s", config_file)
+    logging.debug("troika: %s", troika)
+    logging.debug("troika_config: %s", troika_config)
+    logging.debug("template_job: %s", template_job)
+    logging.debug("task_job: %s", task_job)
+    logging.debug("output: %s", output)
     submission_defs = scheduler.TaskSettings(config.env_submit)
     sub = scheduler.NoSchedulerSubmission(submission_defs)
     sub.submit(
         kwargs.get("task"),
         config,
-        kwargs.get("template_job"),
-        kwargs.get("task_job"),
-        kwargs.get("output"),
-        kwargs.get("troika"),
+        template_job,
+        task_job,
+        output,
+        troika,
         troika_config
     )
 
