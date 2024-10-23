4199d7b02289ab621ce6f257e3c6321446f0b7a6
Trygve Aspelien <trygveasp@met.no>
2024-10-23 09:34:51 +0000

    Add a test

--- tests/smoke/test_main.py
+++ tests/smoke/test_main.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 """Smoke tests."""
+import pytest
+
+from surfexp.cli import pysfxexp
+
+
+@pytest.fixture(scope="module")
+def tmp_directory(tmp_path_factory):
+    """Return a temp directory valid for this module."""
+    return tmp_path_factory.getbasetemp().as_posix()
+
+
+def test_pysfxexp(tmp_directory):
+    argv = [f"{tmp_directory}/out.toml", "name"]
+    pysfxexp(argv=argv)
