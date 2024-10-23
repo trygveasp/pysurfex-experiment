402044149422864082c5cce06b78808f928fcf44
Trygve Aspelien <trygveasp@met.no>
2024-10-23 10:32:24 +0000

    Remove deode as installation dependency

--- tests/smoke/test_main.py
+++ tests/smoke/test_main.py
@@ -5,12 +5,6 @@ import pytest
 from surfexp.cli import pysfxexp
 
 
-@pytest.fixture(scope="module")
-def tmp_directory(tmp_path_factory):
-    """Return a temp directory valid for this module."""
-    return tmp_path_factory.getbasetemp().as_posix()
-
-
-def test_pysfxexp(tmp_directory):
+def test_pysfxexp(tmp_directory, mock_deode):
     argv = [f"{tmp_directory}/out.toml", "name"]
     pysfxexp(argv=argv)
